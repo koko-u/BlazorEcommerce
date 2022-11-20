bfee70e7dc682816bc2ef7118acfb637a8ae6443
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-11-20 10:58:34 +0900

    create product list component

---
+++ BlazorEcommerce.Shared/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorEcommerce.Shared;
+
+public class Product
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string ImageUrl { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+}
